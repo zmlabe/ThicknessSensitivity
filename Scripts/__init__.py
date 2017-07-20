dc9a1b6121b3f8d9adb6218bb29db2433bbddcee
zmlabe <zml5@cornell.edu>
2017-07-20 14:14:03 -0700

    Added scripts

---
+++ Scripts/__init__.py
@@ -0,0 +1,7 @@
+'''
+The Python repository "ThicknessSensitivity" contains the scripts, data, 
+figures, and text for Arctic sea ice thickness perturbation experiments 
+using WACCM4. We are interested in the role of sea ice thickness anomalies
+on the wintertime large-scale atmospheric flow.
+Author: Zachary Labe (zlabe@uci.edu)
+'''
